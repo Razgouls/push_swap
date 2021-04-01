6f752e8d50b37d69697e44b5c5750ead46951bfa
Elie Oliveira <elie@MacMiniElie.local>
2021-04-01 11:03:08 +0200

    fini

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: eoliveir <elie.oliveir@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 18:58:30 by eoliveir          #+#    #+#             */
-/*   Updated: 2021/04/01 09:04:38 by eoliveir         ###   ########.fr       */
+/*   Updated: 2021/04/01 11:01:26 by eoliveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 
 void			ft_parser(t_data *d, char *line);
 int				ft_loop(t_data *d);
-
 
 #endif
--- includes/shared.h
+++ includes/shared.h
@@ -6,7 +6,7 @@
 /*   By: eoliveir <elie.oliveir@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 08:50:19 by eoliveir          #+#    #+#             */
-/*   Updated: 2021/04/01 10:00:07 by eoliveir         ###   ########.fr       */
+/*   Updated: 2021/04/01 11:02:42 by eoliveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@
 # include <time.h>
 # include <sys/time.h>
 # include "../libft/includes/libft.h"
-
-#include "struct.h"
+# include "struct.h"
 
 /*
 ** FT_FILL_STACK
@@ -56,23 +55,23 @@ void				ft_rrr(t_data *d);
 /*
 **	FT_LIST
 */
-void			ft_free_list(t_stack **s);
-t_stack			*ft_get_last_list(t_stack *s);
-t_stack			*ft_new_elem(t_stack *next, t_stack *prev, int nbr);
-int				ft_issorted(t_data *d, t_stack *a);
-void			ft_free_copy(t_data *d, t_stack *tmp);
-void			ft_add_front_list(t_data *d, int id, t_stack **s, int nbr);
-void			ft_add_back_list(t_data *d, int id, t_stack **s, int nbr);
-void			ft_add_back_list_bis(t_stack **s, int nbr);
-void			ft_copy_list(t_stack **dest, t_stack *src);
-void			ft_delete_elem_first(t_data *d, t_stack **s, int id);
+void				ft_free_list(t_stack **s);
+t_stack				*ft_get_last_list(t_stack *s);
+t_stack				*ft_new_elem(t_stack *next, t_stack *prev, int nbr);
+int					ft_issorted(t_data *d, t_stack *a);
+void				ft_free_copy(t_data *d, t_stack *tmp);
+void				ft_add_front_list(t_data *d, int id, t_stack **s, int nbr);
+void				ft_add_back_list(t_data *d, int id, t_stack **s, int nbr);
+void				ft_add_back_list_bis(t_stack **s, int nbr);
+void				ft_copy_list(t_stack **dest, t_stack *src);
+void				ft_delete_elem_first(t_data *d, t_stack **s, int id);
 
 /*
 ** FT_UTILS
 */
-void			ft_setup_len(t_data *d, int id, char op);
-void			ft_memdel(void *ptr);
-int				ft_check_number(char *str);
-void			ft_print_stack(t_stack *s);
+void				ft_setup_len(t_data *d, int id, char op);
+void				ft_memdel(void *ptr);
+int					ft_check_number(char *str);
+void				ft_print_stack(t_stack *s);
 
 #endif
